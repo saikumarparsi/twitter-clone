a6d114c831f87f5be573a81606b866fe7920d9e3
saikumar <saikumarparsi2001@gmail.com>
2025-01-17 18:14:42 +0530

    3-suggested users fetched

--- backend/server.js
+++ backend/server.js
@@ -16,12 +16,13 @@ cloudinary.config({
 	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
 	api_key: process.env.CLOUDINARY_API_KEY,
 	api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+  });
+  
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); // to parse req.body
+app.use(express.json({limit: "5mb"})); // to parse req.body
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 
 app.use(cookieParser());
