90f6cd8f1759aa72b0f79576d4ec606484f5ec42
saikumar <saikumarparsi2001@gmail.com>
2025-01-17 19:25:38 +0530

    3-like post mutation added

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -33,8 +33,38 @@ const Post = ({ post }) => {
 		}
 	})
 
+	const {mutate: likePost, isPending: isLiking} = useMutation({
+		mutationFn: async()=>{
+			try {
+				const res = await fetch(`api/posts/like/${post._id}`, {
+					method: "POST"
+				})
+				const data = res.json()
+				if(!res.ok) throw new Error(data.error || "something went wrong")
+				return data
+			} catch (error) {
+				throw new Error(error)
+			}
+		},
+		onSuccess: (updatedLikes)=>{
+			toast.success("post liked successfully")
+			// queryClient.invalidateQueries({queryKey: ["posts"]})
+			queryClient.setQueryData(["posts"], (oldData)=>{
+				return oldData.map(p => {
+					if(p._id === post._id){
+						return { ...p, likes: updatedLikes}
+					}
+					return p
+				})
+			})
+		},
+		onError: (error)=>{
+			toast.error(error)
+		}
+	})
+
 	const postOwner = post.user;
-	const isLiked = false;
+	const isLiked = post.likes.includes(authUser._id );
 
 	const isMyPost = authUser._id === post.user._id;
 
@@ -50,7 +80,10 @@ const Post = ({ post }) => {
 		e.preventDefault();
 	};
 
-	const handleLikePost = () => {};
+	const handleLikePost = () => {
+		if(isLiking) return;
+		likePost()
+	};
 
 	return (
 		<>
@@ -159,14 +192,15 @@ const Post = ({ post }) => {
 								<span className='text-sm text-slate-500 group-hover:text-green-500'>0</span>
 							</div>
 							<div className='flex gap-1 items-center group cursor-pointer' onClick={handleLikePost}>
-								{!isLiked && (
+								{isLiking && <LoadingSpinner size="sm"/>}
+								{!isLiked && !isLiking &&  (
 									<FaRegHeart className='w-4 h-4 cursor-pointer text-slate-500 group-hover:text-pink-500' />
 								)}
-								{isLiked && <FaRegHeart className='w-4 h-4 cursor-pointer text-pink-500 ' />}
+								{isLiked && !isLiking && <FaRegHeart className='w-4 h-4 cursor-pointer text-pink-500 ' />}
 
 								<span
-									className={`text-sm text-slate-500 group-hover:text-pink-500 ${
-										isLiked ? "text-pink-500" : ""
+									className={`text-sm group-hover:text-pink-500 ${
+										isLiked ? "text-pink-500" : " text-slate-500"
 									}`}
 								>
 									{post.likes.length}
